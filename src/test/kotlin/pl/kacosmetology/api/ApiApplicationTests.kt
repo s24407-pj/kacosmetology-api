21892d4e976e0b0108edb8966447ebfe7b200673
Michal <s24407@pjwstk.edu.pl>
2024-12-18 18:01:54 +0100

    Init project

---
+++ src/test/kotlin/pl/kacosmetology/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.kacosmetology.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
