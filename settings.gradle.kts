8bba92d4aa180b1399001eb5a879a0e7f6fed26e
Michal <s24407@pjwstk.edu.pl>
2024-12-19 11:29:38 +0100

    accept gradle build scan terms

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "kacosmetology-api"
+
+plugins {
+    id("com.gradle.develocity") version ("3.19")
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
